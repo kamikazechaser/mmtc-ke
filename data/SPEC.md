fe640791ed6b487ccaac7a2baf9a03ce69823127
GochoMugo <mugo@forfuture.co.ke>
2016-03-11 15:10:15 +0300

    Apply new data file specification

---
+++ data/SPEC.md
@@ -0,0 +1,69 @@
+# Data File Format Specification
+
+|Aspect|Detail|
+|------|------|
+|Version|0.0|
+|Written by|GochoMugo <mugo@forfuture.co.ke>|
+
+The data used in the application in its computations is fed through data files
+placed in this directory. This allows the application logic code remain
+independent of the different networks available.
+
+## spec:
+
+The data files are written in JSON format.
+
+A single file can only hold one [Network](#type-network) object.
+
+<a name="type-network"></a>
+### Network
+
+|Key|Type|Description|
+|---|----|-----------|
+|name|[Name](#type-name)|The name of the Network|
+|transactions|[Transaction](#type-transaction)|The transactions supported by the network|
+
+<a name="type-transaction"></a>
+### Transaction
+
+|Key|Type|Description|
+|---|----|-----------|
+|name|[Name](#type-name)|Type of the transaction|
+|classes|[Class](#type-class)|The different classes of the transaction|
+
+<a name="type-class"></a>
+### Class
+
+|Key|Type|Description|
+|---|----|-----------|
+|name|[Name](#type-name)|Name of the class of the transaction|
+|ranges|[Range](#type-range)[]|The ranges in the transaction class|
+|amount|boolean|`false` if amount is **not** applicable to the class. Otherwise `false`. Default is `true`|
+
+
+### Range
+<a name="type-range"></a>
+
+|Key|Type|Description|
+|---|----|-----------|
+|low|[Cost](#type-cost)|Lower-bound of the Range|
+|high|[Cost](#type-cost)|Upper-bound of the Range|
+|amount|[Cost](#type-cost)|Amount charged to user|
+
+
+### Cost
+<a name="type-cost"></a>
+
+This type is based on the native `Number` type in JSON with these few additions:
+
+* `"-Infinity"`: implies `-Infinity`
+* `"+Infinity"`: implies `+Infinity`
+* **no** fractional part
+
+Therefore, the cost is accurate to **1 KES**.
+
+
+<a name="type-name"></a>
+### Name
+
+This is a string, **uniquely** identifying the named object.
--- engine/index.js
+++ engine/index.js
@@ -5,18 +5,31 @@ exports = module.exports = {
   getNetworks: getNetworks,
 };
 
+
+// built-in modules
 var fs = require('fs');
 var path = require('path');
 
+
+// npm-installed modules
 var _ = require('lodash');
 
+
+// module variables
 var datadir = path.resolve(__dirname, '../data');
 var cache = {
   networks: {},
 };
+var isJsonFileRegex = new RegExp('.json$');
 
+
+/**
+ * Initialize the Engine. It executes synchronously.
+ */
 function init() {
   fs.readdirSync(datadir).forEach(function(filename) {
+    if (!isJsonFileRegex.test(filename)) return;
+
     var filepath = path.join(__dirname, '../data', filename);
     var data = require(filepath);
     cache.networks[data.name] = data;
@@ -24,64 +37,84 @@ function init() {
 }
 
 
+/**
+ * Return names of all the loaded networks in an array.
+ *
+ * @return {Array}
+ */
 function getNetworks() {
   var networks = [];
+
   for (var networkName in cache.networks) {
     networks.push(cache.networks[networkName]);
   }
+
   return networks;
 }
 
 
+/**
+ * Return the network object.
+ *
+ * @param {String} name of the network (possibly from `getNetworks()`)
+ * @return {Network}
+ */
 function getNetwork(name) {
   return cache.networks[name];
 }
 
+
 /**
- * Calculate the amount
+ * Calculate the Cost.
  *
  * @param {String} name of the network
  * @param {Object} params of the calculation
  * @param {String} params.transactionType e.g. 'transfer', 'withdrawal'
  * @param {String} params.transactor e.g. 'atm', 'registered users'
  * @param {String|Number} params.amount of the transaction
+ * @return {Cost}
+ * @throws NetworkNotFoundError if no such network has been loaded
+ * @throws TransactionNotFoundError if the target transaction was not found
+ * @throws TransactionClassNotFoundError if the target class of the
+ *  transaction was not found
+ * @throws RangeNotFoundError if the amount was not found in any range
+ * @throws InvalidAmountError if the amount entered was invalid
  */
 function calculate(name, params) {
   var network, transaction, transactionClass;
 
   network = getNetwork(name);
   if (!network) {
-    throw new Error('NetworkNotFound');
+    throw new Error('NetworkNotFoundError');
   }
 
   transaction = network.transactions.find(function(t) {
-    return params.transactionType === t.type;
+    return params.transactionType === t.name;
   });
   if (!transaction) {
-    throw new Error('TransactionNotFound');
+    throw new Error('TransactionNotFoundError');
   }
 
   transactionClass = transaction.classes.find(function(c) {
-    return params.transactor === c.transactor;
+    return params.transactor === c.name;
   });
   if (!transactionClass) {
-    throw new Error('TransactionClassNotFound');
+    throw new Error('TransactionClassNotFoundError');
   }
 
   var amount, range;
 
   amount = parseInt(params.amount, 10);
   if (amount < 0) {
-    throw new Error('InvalidAmount');
+    throw new Error('InvalidAmountError');
   }
 
   range = transactionClass.ranges.find(function(r) {
     r = parseRange(r);
     return r !== null && r.low <= amount && amount <= r.high;
   });
-
   if (!range) {
-    throw new Error('RangeNotFound');
+    throw new Error('RangeNotFoundError');
   }
 
   return range.amount;
@@ -89,7 +122,7 @@ function calculate(name, params) {
 
 
 /**
- * Parse the range into valid numerical values
+ * Parse the range into an internal object.
  *
  * @param {Object} range
  * @param {String|Number} range.low
@@ -105,7 +138,6 @@ function parseRange(range) {
     high: _parse(range.high),
     amount: _parse(range.amount),
   };
-
   if (_.isNumber(result.low) && _.isNumber(result.high) && _.isNumber(range.amount)) {
     return result;
   }
