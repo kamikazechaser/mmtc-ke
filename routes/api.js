b2136cffce7d2d98c01bb991b14b19fd4c11c0d1
GochoMugo <mugo@forfuture.co.ke>
2016-04-23 09:28:55 +0300

    [routes/api] Add endpoints for network data
    The endpoints '/networks' and '/networks/:network' while provide
    data from the relevant data files.
    
    issue: #8

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,51 @@
+// built-in modules
+var path = require("path");
+
+
 // npm-installed modules
 var Debug = require("debug");
 var express = require("express");
 
 
+// own modules
+var engine = require("../engine");
+
+
 // module variables
 var debug = Debug("mmtc-ke:routes:api");
 var router = express.Router();
+var logger = engine.clients.getLogger();
 
 
-// just say hello
-router.use("/", function(req, res) {
-    return res.json({ message: "Hello, World!" });
+// serving data for all networks
+router.get("/networks", function(req, res, next) {
+  return res.json({
+    networks: engine.networks.getNetworks(),
+  });
+});
+
+
+// serving data for a network
+router.get("/networks/:network", function(req, res, next) {
+  var network = engine.networks.getNetwork(req.params.network);
+  if (!network) {
+    var networkNotFoundError = new engine.errors.NetworkNotFoundError(`network '${req.params.network}' not found`);
+    networkNotFoundError.status_code = 404;
+    return next(networkNotFoundError);
+  }
+  return res.json(network);
+});
+
+
+// API Error handler
+router.use(function(err, req, res, next) {
+  var status_code = err.status_code || 500;
+  if (status_code === 500) {
+    logger.error(err);
+  }
+  return res.status(status_code).json({
+    error: err,
+  });
 });
 
 
